09c7949d1547209e97140c15dd1b27e18b86cbf9
Jerome Cheon <cheonjh4572@gmail.com>
2026-06-18 10:56:46 +0900

    feature: Add a basic spring boot 4 practice project

---
+++ springboot4-practice/src/main/java/me/jeromecheon/springboot4practice/Springboot4PracticeApplication.java
@@ -0,0 +1,13 @@
+package me.jeromecheon.springboot4practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot4PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot4PracticeApplication.class, args);
+    }
+
+}
---
+++ springboot4-practice/src/test/java/me/jeromecheon/springboot4practice/Springboot4PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jeromecheon.springboot4practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot4PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

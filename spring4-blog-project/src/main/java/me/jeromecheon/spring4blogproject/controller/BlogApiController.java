28b77d59d6a3636afcc1e4302e61beeaa6f818e1
Jerome Cheon <cheonjh4572@gmail.com>
2026-06-21 16:05:36 +0900

    feat: Add article create API

---
+++ spring4-blog-project/src/main/java/me/jeromecheon/spring4blogproject/controller/BlogApiController.java
@@ -0,0 +1,26 @@
+package me.jeromecheon.spring4blogproject.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.jeromecheon.spring4blogproject.domain.Article;
+import me.jeromecheon.spring4blogproject.dto.AddArticleRequest;
+import me.jeromecheon.spring4blogproject.service.BlogService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class BlogApiController {
+  private final BlogService blogService;
+
+  @PostMapping("/api/articles")
+  public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
+    Article savedArticle = this.blogService.save(request);
+
+    return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(savedArticle);
+  }
+}
---
+++ spring4-blog-project/src/main/java/me/jeromecheon/spring4blogproject/dto/AddArticleRequest.java
@@ -0,0 +1,22 @@
+package me.jeromecheon.spring4blogproject.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.jeromecheon.spring4blogproject.domain.Article;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class AddArticleRequest {
+  private String title;
+  private String content;
+
+  public Article toEntity() {
+    return Article.builder()
+            .title(this.title)
+            .content(this.content)
+            .build();
+  }
+}

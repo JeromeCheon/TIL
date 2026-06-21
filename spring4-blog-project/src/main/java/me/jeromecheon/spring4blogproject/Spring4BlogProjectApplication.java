3ba01bbed4da6f8c8feaa78780a8db9817a1b249
Jerome Cheon <cheonjh4572@gmail.com>
2026-06-21 14:39:06 +0900

    chore: Initialize spring4-blog-project

---
+++ spring4-blog-project/src/main/java/me/jeromecheon/spring4blogproject/Spring4BlogProjectApplication.java
@@ -0,0 +1,13 @@
+package me.jeromecheon.spring4blogproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring4BlogProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Spring4BlogProjectApplication.class, args);
+  }
+
+}
---
+++ spring4-blog-project/src/test/java/me/jeromecheon/spring4blogproject/Spring4BlogProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jeromecheon.spring4blogproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring4BlogProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

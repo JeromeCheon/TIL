f9a0c8bf29c375ee02d8cc8db7ef145e368ca8ce
Jerome Cheon <cheonjh4572@gmail.com>
2026-06-26 16:25:48 +0900

    feat: Add RefreshToken and filter

---
+++ spring4-blog-project/src/main/java/me/jeromecheon/spring4blogproject/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package me.jeromecheon.spring4blogproject.repository;
+
+import me.jeromecheon.spring4blogproject.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+  Optional<RefreshToken> findByUserId(Long userId);
+  Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}

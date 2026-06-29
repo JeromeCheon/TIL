8fe6350b252faa395ef699060c611ccebed2b50d
Jerome Cheon <cheonjh4572@gmail.com>
2026-06-29 22:08:15 +0900

    feat: Add nickname field to User and OAuth2 custom user service

--- spring4-blog-project/src/main/java/me/jeromecheon/spring4blogproject/domain/User.java
+++ spring4-blog-project/src/main/java/me/jeromecheon/spring4blogproject/domain/User.java
@@ -25,10 +25,14 @@ public class User implements UserDetails {
   @Column(name = "password")
   private String password;
 
+  @Column(name = "nickname", unique = true)
+  private String nickname;
+
   @Builder
-  public User(String email, String password, String auth) {
+  public User(String email, String password, String nickname) {
     this.email = email;
     this.password = password;
+    this.nickname = nickname;
   }
 
   @Override
@@ -63,5 +67,11 @@ public class User implements UserDetails {
   @Override
   public boolean isEnabled() {
     return true;
+  }
+
+  public User update(String nickname) {
+    this.nickname = nickname;
+
+    return this;
   }
 }

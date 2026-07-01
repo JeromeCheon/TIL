ce68e704854896cd27f7becd4d8c985bacbc3add
Jerome Cheon <cheonjh4572@gmail.com>
2026-07-01 14:53:17 +0900

    feat: Redirect authenticated users away from login page

--- spring4-blog-project/src/main/java/me/jeromecheon/spring4blogproject/controller/UserViewController.java
+++ spring4-blog-project/src/main/java/me/jeromecheon/spring4blogproject/controller/UserViewController.java
@@ -1,12 +1,24 @@
 package me.jeromecheon.spring4blogproject.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import me.jeromecheon.spring4blogproject.config.jwt.TokenProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.util.WebUtils;
 
+@RequiredArgsConstructor
 @Controller
 public class UserViewController {
+  private final TokenProvider tokenProvider;
+
   @GetMapping("/login")
-  public String login() {
+  public String login(HttpServletRequest request) {
+    Cookie refreshTokenCookie = WebUtils.getCookie(request, "refresh_token");
+    if (refreshTokenCookie != null && tokenProvider.validToken(refreshTokenCookie.getValue())) {
+      return "redirect:/articles";
+    }
     return "oauthLogin";
   }
 

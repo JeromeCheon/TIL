19905328dacf777265e042f5813217cc0260fff8
Jerome Cheon <cheonjh4572@gmail.com>
2026-07-02 00:30:38 +0900

    fix: Remove onclick attribute blocking logout and dead session-based handler

--- spring4-blog-project/src/main/java/me/jeromecheon/spring4blogproject/controller/UserApiController.java
+++ spring4-blog-project/src/main/java/me/jeromecheon/spring4blogproject/controller/UserApiController.java
@@ -1,14 +1,9 @@
 package me.jeromecheon.spring4blogproject.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import me.jeromecheon.spring4blogproject.dto.AddUserRequest;
 import me.jeromecheon.spring4blogproject.service.UserService;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -19,13 +14,6 @@ public class UserApiController {
   @PostMapping("/user")
   public String signup(AddUserRequest request) {
     this.userService.save(request);
-    return "redirect:/login";
-  }
-
-  @GetMapping()
-  public String logout(HttpServletRequest request, HttpServletResponse response) {
-    new SecurityContextLogoutHandler().logout(request, response,
-            SecurityContextHolder.getContext().getAuthentication());
     return "redirect:/login";
   }
 }

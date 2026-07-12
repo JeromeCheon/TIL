e75e1aac142c60d741c9c1f8220342aca3d66147
Jerome Cheon <cheonjh4572@gmail.com>
2026-07-12 20:47:10 +0900

    feat: Add DTO validation for Article creation

--- spring4-blog-project/src/main/java/me/jeromecheon/spring4blogproject/dto/AddArticleRequest.java
+++ spring4-blog-project/src/main/java/me/jeromecheon/spring4blogproject/dto/AddArticleRequest.java
@@ -1,6 +1,8 @@
 package me.jeromecheon.spring4blogproject.dto;
 
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +12,12 @@ import me.jeromecheon.spring4blogproject.domain.Article;
 @AllArgsConstructor
 @Getter
 public class AddArticleRequest {
+
+  @NotNull
+  @Size(min = 1, max = 10)
   private String title;
+
+  @NotNull
   private String content;
 
   public Article toEntity(String author) {

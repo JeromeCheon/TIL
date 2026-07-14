8cd825f5f5ffdc5c4801903db836cfed17c323e6
Jerome Cheon <cheonjh4572@gmail.com>
2026-07-14 18:01:31 +0900

    feat: Add comment feature to articles

--- spring4-blog-project/src/main/java/me/jeromecheon/spring4blogproject/service/BlogService.java
+++ spring4-blog-project/src/main/java/me/jeromecheon/spring4blogproject/service/BlogService.java
@@ -4,9 +4,12 @@ package me.jeromecheon.spring4blogproject.service;
 import lombok.RequiredArgsConstructor;
 import me.jeromecheon.spring4blogproject.config.error.exception.ArticleNotFoundException;
 import me.jeromecheon.spring4blogproject.domain.Article;
+import me.jeromecheon.spring4blogproject.domain.Comment;
 import me.jeromecheon.spring4blogproject.dto.AddArticleRequest;
+import me.jeromecheon.spring4blogproject.dto.AddCommentRequest;
 import me.jeromecheon.spring4blogproject.dto.UpdateArticleRequest;
 import me.jeromecheon.spring4blogproject.repository.BlogRepository;
+import me.jeromecheon.spring4blogproject.repository.CommentRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +20,7 @@ import java.util.List;
 @Service
 public class BlogService {
   private final BlogRepository blogRepository;
+  private final CommentRepository commentRepository;
 
   public Article save(AddArticleRequest request, String userName) {
     return this.blogRepository.save(request.toEntity(userName));
@@ -48,10 +52,17 @@ public class BlogService {
     return article;
   }
 
+  public Comment addComment(AddCommentRequest request, String userName) {
+    Article article = this.blogRepository.findById(request.getArticleId())
+            .orElseThrow(()-> new IllegalArgumentException("not found: " + request.getArticleId()));
+    return this.commentRepository.save(request.toEntity(userName, article));
+  }
+
   private static void authorizeArticleAuthor(Article article) {
     String userName = SecurityContextHolder.getContext().getAuthentication().getName();
     if (!article.getAuthor().equals(userName)) {
       throw new IllegalArgumentException("not authorized");
     }
   }
+
 }
--- spring4-blog-project/src/main/resources/import.sql
+++ spring4-blog-project/src/main/resources/import.sql
@@ -4,3 +4,8 @@ INSERT INTO article (title, content, author, created_at, updated_at)
 VALUES ('제목 2', '내용 2', 'user2', NOW(), NOW());
 INSERT INTO article (title, content, author, created_at, updated_at)
 VALUES ('제목 3', '내용 3', 'user3', NOW(), NOW());
+
+INSERT INTO comments (article_id, author, content, created_at)
+VALUES (1, 'user4', '댓글 1', NOW());
+INSERT INTO comments (article_id, author, content, created_at)
+VALUES (1, 'user5', '댓글 2', NOW());
